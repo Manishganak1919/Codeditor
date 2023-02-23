2194949717659b0619c370597172e0db2f4cfde3
Manish kumar <98228493+Manishganak1919@users.noreply.github.com>
2023-02-23 20:39:45 +0530

    code-pen-code
    This is code-pen clone

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+//component
+import Header from "./Header";
+import Code from "./Code";
+import Result from "../Result";
+const Home=()=>{
+  return(
+    <>
+      <Header/>
+      <Code/>
+      <Result/>
+    </>
+  )
+}
+export default Home;
